4d14719f8e436d8e3fba3341d3d4ed29746cc7e6
Mehdi <mfadhloune@gmail.com>
2025-09-11 19:44:41 +0200

    possibilité s'ajouter un dresseur

---
+++ Pokemon App/Controller/AjoutDresseurViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AjoutDresseurViewController.swift
+//  Pokemon App
+//
+//  Created by Mehdi FADHLOUNE on 9/11/25.
+//
+
+import UIKit
+
+class AjoutDresseurViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
